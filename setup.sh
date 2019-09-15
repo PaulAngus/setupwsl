aca4cd62b3babcdd083cbe15fb14400ca46288e5
PaulAngus <paul.angus@shapeblue.com>
2019-09-15 19:53:54 +0100

    add utils

--- setup.sh
+++ setup.sh
@@ -5,6 +5,9 @@ pip3 install sphinx_rtd_theme docopt
 gem install middleman
 sudo gem install middleman
 bundler install
+wget https://github.com/apache/cloudstack-cloudmonkey/releases/download/pre-next-rc/cmk.linux.x86-64
+mv /.cmk.linux.x86-64 /usr/bin/cmk
+chmod +x /usr/bin/cmk
 echo "alias docbuild='sphinx-build -b html /mnt/c/Users/paul/Documents/Git/cloudstack-documentation/source /mnt/c/Users/paul/Documents/Git/acsdocs_built'
 alias gotogit='cd /mnt/c/Users/paul/Documents/Git/'
 alias wwwbuild='bundle exec middleman build && cp -r ./build/* /mnt/c/Users/paul/Documents/Git/acswww-built'
